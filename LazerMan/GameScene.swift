e4335e80e1c295bec7815a011696249bc3b8ed44
Tye Blackie <blackie.tye@gmail.com>
2017-09-07 14:35:51 -0700

    test commit

--- LazerMan/GameScene.swift
+++ LazerMan/GameScene.swift
@@ -11,79 +11,110 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var Hero = SKSpriteNode(imageNamed: "SoccerBall")
+    var background = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let border = self.childNode(withName: "BorderSprite")
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        Hero.size = CGSize(width: 200, height: 200)
+        Hero.position = CGPoint(x: 0, y: -600)
+        Hero.zPosition = 2
+        
+        self.addChild(Hero)
+        
+        createBackgrounds()
+        
+        
+        let borderFrame = SKPhysicsBody(edgeLoopFrom: (border?.frame)!)
+        borderFrame.friction = 0
+        borderFrame.restitution = 1
+        self.physicsBody = borderFrame
+        
+        
+//        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+//        border.friction = 0
+//        border.restitution = 1
+//        self.physicsBody = border
+        
+//        let rightBorder = SKPhysicsBody(edgeFrom: CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), to: CGPoint(x: self.frame.width / 2, y: -(self.frame.height / 2)))
+//        rightBorder.friction = 0
+//        self.physicsBody = rightBorder
+//        
+//        let leftBorder = SKPhysicsBody(edgeFrom: CGPoint(x: -(self.frame.width / 2), y: self.frame.height / 2), to: CGPoint(x: -(self.frame.width / 2), y: -(self.frame.height / 2)))
+//        leftBorder.friction = 0
+//        self.physicsBody = leftBorder
+    }
+    
+    
+    
+    
+    
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        moveBackgrounds()
+    }
+    
+    func createBackgrounds() {
+        
+        for i in 0...3 {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            let background = SKSpriteNode(imageNamed: "MetalBackground")
+            background.name = "Background"
+            background.size = CGSize(width: (self.scene?.size.width)!, height: 1000)
+            background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            background.position = CGPoint(x: 0, y: CGFloat(i) * background.size.height)
+            background.zPosition = 0
+            
+            self.addChild(background)
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    func moveBackgrounds() {
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        self.enumerateChildNodes(withName:"Background") { (node, error) in
+            
+            node.position.y -= 1
+            
+            if node.position.y < -((self.scene?.size.height)!) {
+                
+                node.position.y += (self.scene?.size.height)! * 3
+            }
+        }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.location(in: self)
+            
+            var SmallBall = SKSpriteNode(imageNamed: "SoccerBall")
+            SmallBall.position = Hero.position
+            SmallBall.size = CGSize(width: 30, height: 30)
+            SmallBall.physicsBody = SKPhysicsBody(circleOfRadius: SmallBall.size.width / 2)
+            SmallBall.physicsBody?.affectedByGravity = true
+            SmallBall.zPosition = 1
+            
+            self.addChild(SmallBall)
+            
+            
+        var dx = CGFloat(location.x - Hero.position.x)
+        var dy = CGFloat(location.y - Hero.position.y)
+            
+            let magnitude = sqrt(dx * dx + dy * dy)
+            
+            dx /= magnitude
+            dy /= magnitude
+            
+            let vector = CGVector(dx: 60.0 * dx, dy: 60.0 * dy)
+            
+            
+        SmallBall.physicsBody?.applyImpulse(vector)
+            
+            
+            
+        }
     }
 }
